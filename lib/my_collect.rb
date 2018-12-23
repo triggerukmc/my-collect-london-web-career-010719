5e9c0adef2e89ef02efdf748b1c976517a98b44f
Matthew Collins <matthew.collins1991@yahoo.com>
2018-12-23 15:31:42 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,8 @@
 def my_collect(language)
   iteration = 0
   while iteration < language.length
-  yield(language[iteration]) do |lang|
-  lang.upcase
-end
+  yield(language[iteration]) do {|lang|
+  lang.upcase}
 end
 language
 end
